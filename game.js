0c295dec8e3995211642f9592e0e1bbebd4e466f
unknown <paulzaitsev@gmail.com>
2025-03-24 23:57:36 -0400

    add work in progress logic

--- game.js
+++ game.js
@@ -8,3 +8,10 @@ gamePattern.push(randomChosenColor);
 function nextSequence(){
     return Math.floor( Math.random()*4 );;
 }
+
+$("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+
+var audio = new Audio("./sounds/"+randomChosenColor+'.mp3');
+audio.play();
+
+$("")
